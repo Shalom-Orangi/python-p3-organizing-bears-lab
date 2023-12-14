79cedd1e6a099f7cc253986809e0ab10c5cd0ff4
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-12-15 00:50:56 +0300

    Update

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+('Mr. Chocolate', 5,'M','Brown','shy','Yes'),
+('Rowdy',9, 'M', 'Black''sensitive','Yes'),
+('Tabitha', 4,'F', 'Brown','easy','Yes'),
+('Sergeant Brown', 9,'M', 'Brown','difficult','No'),
+('Melissa', 3,'F', 'Black', 'shy','No'),
+('Grinch', 6,'M', 'Green','difficult','Yes'),
+('Wendy',8, 'F', 'Brown','sensitive','Yes'),
+(NULL, 7,'M', 'Black','easy','No'); 
